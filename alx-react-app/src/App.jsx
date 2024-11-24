31494f52bde7bda2c033ca2b984d7cf5da350aa3
Sphiwe Mabena <nokuthulamabena31@gmail.com>
2024-11-24 22:52:52 +0200

    alx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,16 @@
 // src/App.jsx
 import React from 'react';
 import WelcomeMessage from './components/WelcomeMessage';  // Import the new component
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <MainContent />
+      <Footer />
+      <Header />
       <WelcomeMessage />  {/* Include the new component here */}
     </div>
   );
