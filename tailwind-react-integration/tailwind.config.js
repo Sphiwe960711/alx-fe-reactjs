2ee7bbe4898dc79607117c51eab6f1c41bf65504
Sphiwe960711 <snmabena@icloud.com>
2024-12-17 01:05:04 +0200

    Update tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.js
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,ts,jsx,tsx}", // Adjust this path based on your file structure
   ],
   theme: {
     extend: {},
